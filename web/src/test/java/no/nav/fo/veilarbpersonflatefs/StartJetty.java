81c64b955bdd291d23799d2e289ff3c3e3bfd3c3
Aksel Wester <aksel.wester@nav.no>
2017-02-06 09:41:36 +0100

    Endrer port

--- web/src/test/java/no/nav/fo/veilarbpersonflatefs/StartJetty.java
+++ web/src/test/java/no/nav/fo/veilarbpersonflatefs/StartJetty.java
@@ -7,7 +7,7 @@ import static no.nav.sbl.dialogarena.common.jetty.JettyStarterUtils.*;
 
 public class StartJetty {
 
-    private static final int PORT = 8487;
+    private static final int PORT = 8485;
 
     public static void main(String []args) {
         Jetty jetty = usingWar()
