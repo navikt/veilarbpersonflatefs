df17618592ba1cd39386ded7d15616c1e110f163
Aksel Wester <aksel.wester@nav.no>
2017-02-08 15:16:02 +0100

    PK-37609 PKULV-2217 Legger til at url oppdaterer seg når event sendes fra hodet

--- web/src/frontend/app/index.jsx
+++ web/src/frontend/app/index.jsx
@@ -3,7 +3,41 @@ import { render } from 'react-dom';
 
 import Personoversikt from 'Personoversikt';
 
+const BASE_URL = '/veilarbpersonflatefs/';
+
 document.addEventListener('DOMContentLoaded', () => {
     render(<Personoversikt />
         , document.getElementById('app'));
 });
+
+const settPersonIURL = (personnummer) => {
+    window.history.pushState(`Endret personnummer til ${personnummer}`, "", `${BASE_URL}${personnummer}`)
+};
+
+const personErSattIURL = () => {
+    const url = window.location.pathname;
+    const regex = new RegExp(/^/.source + BASE_URL + /\d+\/?$/.source);
+    return url.match(regex);
+};
+
+const hentPersonnummerFraURL = () => {
+    const url = window.location.pathname;
+    const regex = new RegExp(/^/.source + BASE_URL + /(\d+)\/?$/.source);
+    const match = url.match(regex);
+    if (match.length == 2) {
+        return match[1];
+    }
+    return null;
+};
+
+document.addEventListener('dekorator-hode-personsok', (event) => {
+    settPersonIURL(event.personnummer);
+});
+
+window.onload = () => {
+    if (personErSattIURL()) {
+        let personsokEvent = new Event('dekorator-hode-personsok');
+        personsokEvent.personnummer = hentPersonnummerFraURL();
+        document.dispatchEvent(personsokEvent);
+    }
+};
