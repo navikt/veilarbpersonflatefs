c38c054b70f980f23db3f1eb3deb4de37182b4c0
Joakim Lindquister <joakim.lindquister@nav.no>
2017-02-09 13:21:45 +0100

    PK-37609 Legg til flatens egen meny i hodet
    Henger oss på klikk eventet på menyknappen i hodet og injecter vår egen meny-html.

--- web/src/frontend/app/index.jsx
+++ web/src/frontend/app/index.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
+    const hodeMeny = document.getElementById('js-dekorator-nav');
+    const veilarbpersonflatefsMeny = document.getElementById('veilarbpersonflatefs-meny');
+    const hodeMenyKnapp = document.getElementById('js-dekorator-toggle-meny');
+    hodeMenyKnapp.addEventListener('click', () => {
+        hodeMeny.innerHTML = veilarbpersonflatefsMeny.innerHTML;
+    });
     render((
         <h3>Hallo VeilArbPersonFlateFS!</h3>
     ), document.getElementById('app'));
