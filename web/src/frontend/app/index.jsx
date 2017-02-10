2c0fccfa377c613b7931cfeb371e4feea9184506
Aksel Wester <aksel.wester@nav.no>
2017-02-10 10:23:06 +0100

    PK-37609 PKULV-2217 Flytter eventhåndteringen til en egen fil

--- web/src/frontend/app/index.jsx
+++ web/src/frontend/app/index.jsx
@@ -2,46 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import Personoversikt from 'Personoversikt'; // eslint-disable-line
-
-const BASE_URL = '/veilarbpersonflatefs/';
+import { initialiserEventhandtering } from './eventhandtering';
 
 document.addEventListener('DOMContentLoaded', () => {
     render(<Personoversikt />
         , document.getElementById('app'));
 });
 
-const settPersonIURL = (personnummer) => {
-    window.history.pushState(`Endret personnummer til ${personnummer}`, '', `${BASE_URL}${personnummer}`);
-};
-
-const personErSattIURL = () => {
-    const url = window.location.pathname;
-    const regex = new RegExp(/^/.source + BASE_URL + /\d+\/?$/.source);
-    return url.match(regex);
-};
-
-const hentPersonnummerFraURL = () => {
-    const url = window.location.pathname;
-    const regex = new RegExp(/^/.source + BASE_URL + /(\d+)\/?$/.source);
-    const match = url.match(regex);
-    if (match.length === 2) {
-        return match[1];
-    }
-    return null;
-};
-
-const sendEventOmPersonFraURL = () => {
-    if (personErSattIURL()) {
-        const personsokEvent = new CustomEvent('flate-person-endret'); // eslint-disable-line no-undef
-        personsokEvent.personnummer = hentPersonnummerFraURL();
-        document.dispatchEvent(personsokEvent);
-    }
-};
-
-window.onload = sendEventOmPersonFraURL;
-window.onpopstate = sendEventOmPersonFraURL;
-
-document.addEventListener('dekorator-hode-personsok', (event) => {
-    settPersonIURL(event.personnummer);
-    sendEventOmPersonFraURL();
-});
+initialiserEventhandtering();
