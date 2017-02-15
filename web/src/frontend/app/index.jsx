fd7b60442451b198c9ac9ffd10a5a0c367715cef
Joakim Lindquister <joakim.lindquister@nav.no>
2017-02-15 15:09:12 +0100

    PK-37609 PKULV-2143 Meny i personkontekst
    Trekt ut menyvalg i egen fil med mulighet for placeholders i url'ene.
    Dette lar oss linke til person i kontekst i f.eks modia

--- web/src/frontend/app/index.jsx
+++ web/src/frontend/app/index.jsx
@@ -3,15 +3,15 @@ import { render } from 'react-dom';
 
 import Personoversikt from 'Personoversikt'; // eslint-disable-line
 import { initialiserEventhandtering } from './eventhandtering';
+import { lagHtmlMeny } from './meny-utils';
 
 document.addEventListener('DOMContentLoaded', () => {
-
     const hodeMeny = document.getElementById('js-dekorator-nav');
-    const veilarbpersonflatefsMeny = document.getElementById('veilarbpersonflatefs-meny');
     const hodeMenyKnapp = document.getElementById('js-dekorator-toggle-meny');
     hodeMenyKnapp.addEventListener('click', () => {
-        hodeMeny.innerHTML = veilarbpersonflatefsMeny.innerHTML;
+        hodeMeny.innerHTML = lagHtmlMeny();
     });
+
     render(<Personoversikt />
         , document.getElementById('app'));
 });
