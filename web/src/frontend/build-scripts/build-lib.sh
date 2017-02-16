4f26d9c8eec0e63b9de94fbd37d8f25101c234b3
Aksel Wester <aksel.wester@nav.no>
2017-02-16 13:19:10 +0100

    PK-37609 PKULV-2190 Legger til build-script for å flytte react og redux til js-mappa

---
+++ web/src/frontend/build-scripts/build-lib.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+declare -a libs=("react"
+		 "redux"
+		 "redux-thunk"
+		 "react-redux"
+		)
+
+for lib in "${libs[@]}"
+do
+    echo "Kopierer $lib til ../main/webapp/js/"
+    cp ./node_modules/$lib/dist/$lib.js ../main/webapp/js/
+done
