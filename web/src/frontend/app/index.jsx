5cf385dc09f09e1940d15ac697df395cd6973b33
Joakim Lindquister <joakim.lindquister@nav.no>
2017-03-07 12:10:20 +0100

    PKULV-2334 Hvis ikke hodet er internarbeidsflate er lastet, hvis en feilside

--- web/src/frontend/app/index.jsx
+++ web/src/frontend/app/index.jsx
@@ -7,15 +7,23 @@ import Personoversikt from 'Personoversikt'; // eslint-disable-line
 import { initialiserEventhandtering } from './eventhandtering';
 import { lagHtmlMeny } from './meny-utils';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const hodeMeny = document.getElementById('js-dekorator-nav');
-    const hodeMenyKnapp = document.getElementById('js-dekorator-toggle-meny');
-    hodeMenyKnapp.addEventListener('click', () => {
-        hodeMeny.innerHTML = lagHtmlMeny();
-    });
+var visFeilmelding = function () {
+    window.location = '/veilarbpersonflatefs/static/feilside.html';
+};
 
-    render(<Personoversikt />
-        , document.getElementById('app'));
+document.addEventListener('DOMContentLoaded', () => {
+    if (!document.varsel) {
+        visFeilmelding()
+    } else {
+        const hodeMeny = document.getElementById('js-dekorator-nav');
+        const hodeMenyKnapp = document.getElementById('js-dekorator-toggle-meny');
+        hodeMenyKnapp.addEventListener('click', () => {
+            hodeMeny.innerHTML = lagHtmlMeny();
+        });
+
+        render(<Personoversikt />
+            , document.getElementById('app'));
+    }
 });
 
 initialiserEventhandtering();
