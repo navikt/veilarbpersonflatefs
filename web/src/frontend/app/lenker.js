deb2caa814907a784505652054a43abea17a1965
Peder Korsveien <peder.korsveien@nav.no>
2017-04-26 14:51:16 +0200

    Endre miaUrl

--- web/src/frontend/app/lenker.js
+++ web/src/frontend/app/lenker.js
@@ -1,10 +1,12 @@
 export const modiaTemplateUrl = 'https://modapp{{miljoStreng}}.adeo.no/modiabrukerdialog/person/{{fodselsnummer}}';
+export const miaUrl = 'https://modapp{{miljoStreng}}.adeo.no/mia';
+
 
 export const veilarbpersonflatefsLenker =
     {
         lenker:
         [
-                ['/mia/ledigestillinger', 'Arbeidsmarkedet'],
+                [miaUrl, 'Arbeidsmarkedet'],
                 ['/veilarbportefoljeflatefs/enhet?enhet={{enhet}}&clean', 'Enhetens oversikt'],
                 ['/veilarbportefoljeflatefs/portefolje?enhet={{enhet}}&clean', 'Min oversikt'],
                 [modiaTemplateUrl, 'Modia']
