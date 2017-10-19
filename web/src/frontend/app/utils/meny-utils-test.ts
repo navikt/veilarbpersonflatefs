aa3320c6d084ce27d759dc0a4fb7ce43b02f6368
Johan Rusvik <johan.rusvik@nav.no>
2017-10-19 15:50:22 +0200

    Legger til jest og fikser tester

--- web/src/frontend/app/utils/meny-utils-test.ts
+++ web/src/frontend/app/utils/meny-utils-test.ts
@@ -4,18 +4,17 @@ import { modiaTemplateUrl } from '../lenker';
 
 describe('erstattMiljoPlaceholder', () => {
     it('skal gi riktig lenke i t4', () => {
-        global.window = {
+        (global as any).window = {
             location: {
                 host: 'app-t4.adeo.no'
             }
         };
-
         const lenke = erstattMiljoPlaceholder(modiaTemplateUrl);
         expect(lenke).to.equal('https://modapp-t4.adeo.no/modiabrukerdialog/person/{{fodselsnummer}}');
     });
 
     it('skal gi riktig lenke i prod', () => {
-        global.window = {
+        (global as any).window = {
             location: {
                 host: 'app.adeo.no'
             }
