c7c12e4fb904a4f5c7f9e285f8587f4efe5a219b
Alexander Gård <alexander.gard@nav.no>
2019-03-28 13:33:39 +0100

    FO-2135 La til bakgrunnsfarge på kun aktivitetsplan og fjernet gammel layout

--- src/mock/features.ts
+++ src/mock/features.ts
@@ -1,8 +1,7 @@
-import { Features, NY_LAYOUT_TOGGLE, SPOR_OM_TILBAKEMELDING, TOUR_MODAL_TOGGLE } from '../utils/api';
+import { Features, SPOR_OM_TILBAKEMELDING, TOUR_MODAL_TOGGLE } from '../utils/api';
 import { JSONObject } from 'yet-another-fetch-mock';
 
 export default {
-    [NY_LAYOUT_TOGGLE]: true,
     [TOUR_MODAL_TOGGLE]: true,
     [SPOR_OM_TILBAKEMELDING]: true,
 } as Features & JSONObject;
