d74be746a007475f024cc4aa0dc5488eff7fea7d
Alexander Gård <alex.gaard@hotmail.com>
2019-11-26 16:37:31 +0100

    KAIZEN-0 La til config for tour-modal slik at man kan ha flere modaler samtidig

--- src/mock/features-mock.ts
+++ src/mock/features-mock.ts
@@ -1,8 +1,11 @@
-import { Features, SPOR_OM_TILBAKEMELDING, TOUR_MODAL_TOGGLE, VIS_VEDTAKSSTOTTE } from '../utils/featue-utils';
+import {
+    Features, SPOR_OM_TILBAKEMELDING,
+    TOUR_MODAL_LAST_NED_CV_TOGGLE, VIS_VEDTAKSSTOTTE
+} from '../utils/featue-utils';
 import { JSONObject } from 'yet-another-fetch-mock';
 
 export default {
-    [TOUR_MODAL_TOGGLE]: true,
+    [TOUR_MODAL_LAST_NED_CV_TOGGLE]: true,
     [SPOR_OM_TILBAKEMELDING]: true,
     [VIS_VEDTAKSSTOTTE]: true,
 } as Features & JSONObject;
