fb5f28ffb1ec424fdd8ea320e787e35d589b8988
hakon planke holm <hakon.planke.holm@nav.no>
2019-12-11 13:56:45 +0100

    [FO-2580] ny dialog

--- src/components/side-innhold.tsx
+++ src/components/side-innhold.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import TabMenu, { Tab } from './tab-menu/tab-menu';
-import { Features, VIS_VEDTAKSSTOTTE } from '../utils/featue-utils';
+import {Features, VIS_NY_DIALOG, VIS_VEDTAKSSTOTTE} from '../utils/featue-utils';
 import TilbakemeldingFab from './tilbakemelding/fab/tilbakemelding-fab';
 import { hentSistBesokteTab } from './tab-menu/siste-tab';
 import { TourModalController } from './tour-modal/tour-modal-controller';
@@ -10,24 +10,34 @@ interface SideInnholdLayoutProps {
     visittkort: React.ReactElement;
     mao: React.ReactElement;
     aktivitetsplan: React.ReactElement;
+    dialog?: React.ReactElement
     vedtaksstotte?: React.ReactElement;
     features: Features;
     fnr: string;
 }
 
 export const TAG_AKTIVITETSPLAN = 'aktivitetsplan';
+export const TAG_DIALOG = 'dialog';
 export const TAG_VEDTAKSSTOTTE = 'vedtaksstotte';
 export const TAG_DETALJER = 'detaljer';
 
 class SideInnhold extends React.Component<SideInnholdLayoutProps> {
 
     render () {
-        const { visittkort, aktivitetsplan, vedtaksstotte, mao, features, fnr } = this.props;
+        const { visittkort, aktivitetsplan, dialog, vedtaksstotte, mao, features, fnr } = this.props;
 
-        const tabs: Tab[] = [
-            { tag: TAG_AKTIVITETSPLAN, title: 'Aktivitetsplan og dialog', content: aktivitetsplan },
-            { tag: TAG_DETALJER, title: 'Detaljer', content: mao}
-        ];
+        const tabs: Tab[] = [{ tag: TAG_AKTIVITETSPLAN, title: 'Aktivitetsplan og dialog', content: aktivitetsplan }];
+
+        if (dialog && features[VIS_NY_DIALOG]) {
+            tabs.push({
+                tag: TAG_DIALOG,
+                title: 'Dialog',
+                content: dialog
+            });
+        }
+
+        tabs.push({ tag: TAG_DETALJER, title: 'Detaljer', content: mao});
+
         if (vedtaksstotte && features[VIS_VEDTAKSSTOTTE]) {
             tabs.push({
                 tag: TAG_VEDTAKSSTOTTE,
--- src/mock/features-mock.ts
+++ src/mock/features-mock.ts
@@ -1,6 +1,6 @@
 import {
     Features, SPOR_OM_TILBAKEMELDING,
-    TOUR_MODAL_LAST_NED_CV_TOGGLE, VIS_VEDTAKSSTOTTE
+    TOUR_MODAL_LAST_NED_CV_TOGGLE, VIS_NY_DIALOG, VIS_VEDTAKSSTOTTE
 } from '../utils/featue-utils';
 import { JSONObject } from 'yet-another-fetch-mock';
 
@@ -8,4 +8,5 @@ export default {
     [TOUR_MODAL_LAST_NED_CV_TOGGLE]: true,
     [SPOR_OM_TILBAKEMELDING]: true,
     [VIS_VEDTAKSSTOTTE]: true,
+    [VIS_NY_DIALOG]: true,
 } as Features & JSONObject;
