7727922471cdbba0ed1c6563b76cbdd1105d593c
alexandraback <alexandra1.back@gmail.com>
2020-04-27 10:11:21 +0200

    KAIZEN-0: Fjern featuretoggle VIS_NY_DIALOG

--- src/mock/features-mock.ts
+++ src/mock/features-mock.ts
@@ -2,14 +2,12 @@ import {
 	Features,
 	SPOR_OM_TILBAKEMELDING,
 	TOUR_MODAL_LAST_NED_CV_TOGGLE,
-	TOUR_MODAL_VEDTAKSSTOTTE_LANSERING_TOGGLE,
-	VIS_NY_DIALOG
+	TOUR_MODAL_VEDTAKSSTOTTE_LANSERING_TOGGLE
 } from '../utils/feature-utils';
 import { JSONObject } from 'yet-another-fetch-mock';
 
 export default {
 	[TOUR_MODAL_VEDTAKSSTOTTE_LANSERING_TOGGLE]: true,
 	[TOUR_MODAL_LAST_NED_CV_TOGGLE]: false,
-	[SPOR_OM_TILBAKEMELDING]: true,
-	[VIS_NY_DIALOG]: true
+	[SPOR_OM_TILBAKEMELDING]: true
 } as Features & JSONObject;
