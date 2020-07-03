a53d3d88d40454d9ab5811f93ba226ec823bbb10
alexander-svedsen <alexandersvendsen1990@gmail.com>
2020-07-03 10:27:08 +0200

    Refaktorer effekten i forskjellige metoder

--- src/components/tab-menu/dialog-tab/useAntalUlesteDialoger.ts
+++ src/components/tab-menu/dialog-tab/useAntalUlesteDialoger.ts
@@ -1,5 +1,5 @@
-import {useEffect, useState} from "react";
-import {fetchSistOppdatert, fetchUlesteDialoger} from "../../../utils/api";
+import {useCallback, useEffect, useState} from "react";
+import {fetchSistOppdatert, fetchUlesteDialoger, SistOppdatertData} from "../../../utils/api";
 import {useEventListener} from "../../../utils/utils";
 import {hentFnrFraUrl} from "../../../utils/url-utils";
 
@@ -16,7 +16,7 @@ const eventName = 'uppdate';
 
 export function widowEvent(update: UpdateTypes) {
     window.dispatchEvent(
-        new CustomEvent<UpdateEventType>(eventName, { detail: { uppdate: update, avsender: 'veilarbpersonflatefs' } })
+        new CustomEvent<UpdateEventType>(eventName, {detail: {uppdate: update, avsender: 'veilarbpersonflatefs'}})
     );
 }
 
@@ -27,43 +27,46 @@ export default function useUlesteDialoger(): number | undefined {
     const fnr = hentFnrFraUrl();
     const [localSistOppdatert, setLocalSistOppdatert] = useState(new Date());
 
-    useEffect(() => {
-        if(fnr) {
-            fetchUlesteDialoger(fnr)
-                .then(a => setAntallUleste(a.antallUleste))
-                .catch();
-        }
-    }, [fnr, setAntallUleste]);
+    const fetchAntallUlesteDialoger = useCallback(() => {
+            if (fnr) {
+                fetchUlesteDialoger(fnr)
+                    .then(a => setAntallUleste(a.antallUleste))
+                    .catch()
+            }
+        },
+        [fnr, setAntallUleste]);
 
     useEffect(() => {
-        if(fnr){
+        fetchAntallUlesteDialoger();
+    }, [fetchAntallUlesteDialoger]);
+
+    const oppdaterDialogDataHvisNyere = useCallback((data: SistOppdatertData) => {
+        if (!!data.sistOppdatert) {
+            const remoteSistOppdatert = new Date(data.sistOppdatert);
+            if (remoteSistOppdatert > localSistOppdatert) {
+                widowEvent(UpdateTypes.Dialog);
+                fetchAntallUlesteDialoger();
+                setLocalSistOppdatert(new Date());
+            }
+        }
+    }, [localSistOppdatert, setLocalSistOppdatert, fetchAntallUlesteDialoger]);
+
+    useEffect(() => {
+        if (fnr) {
             let interval: number;
             const pollForChanges = () => fetchSistOppdatert(fnr)
-                .then(data => {
-                    if(!!data.sistOppdatert){
-                        const remoteSistOppdatert = new Date(data.sistOppdatert);
-                        if (remoteSistOppdatert > localSistOppdatert){
-                            widowEvent(UpdateTypes.Dialog);
-
-                            fetchUlesteDialoger(fnr)
-                                .then(a => setAntallUleste(a.antallUleste))
-                                .catch();
-
-                            setLocalSistOppdatert(new Date());
-                        }
-                    }
-                });
+                .then(oppdaterDialogDataHvisNyere);
 
             interval = setInterval(pollForChanges, 10000);
             return () => clearInterval(interval);
         }
-        return () => {};
+        return () => {
+        };
+    }, [fnr, oppdaterDialogDataHvisNyere]);
 
-    }, [fnr, localSistOppdatert, setLocalSistOppdatert, setAntallUleste]);
 
-
-    useEventListener(DIALOG_LEST_EVENT, () =>{
-        setAntallUleste(prevState => prevState? prevState - 1 : 0);
+    useEventListener(DIALOG_LEST_EVENT, () => {
+        setAntallUleste(prevState => prevState ? prevState - 1 : 0);
     });
 
     return antallUleste
