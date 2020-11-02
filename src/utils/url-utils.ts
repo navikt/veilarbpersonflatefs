e872dae0e2538e2ca2273f732f34bc6023bd030b
Alexander Gård <alex.gaard@hotmail.com>
2020-11-02 19:27:25 +0100

    Fjernet ubrukt kode

--- src/utils/url-utils.ts
+++ src/utils/url-utils.ts
@@ -1,6 +1,3 @@
-import * as queryString from 'query-string';
-
-const location = window.location;
 
 export const hasHashParam = (parameterName: string): boolean => {
 	return window.location.hash.includes(parameterName);
@@ -10,40 +7,7 @@ export const hasQueryParam = (parameterName: string): boolean => {
 	return window.location.search.includes(parameterName);
 };
 
-export const refreshMedNyEnhetIUrl = (enhet: string) => {
-	const currentParams = queryString.parse(location.search);
-	currentParams.enhet = enhet;
-	location.search = queryString.stringify(currentParams);
-};
-
-export function hentEnhetIdFraUrl(): string | undefined {
-	const enhet = queryString.parse(location.search).enhet;
-
-	if (Array.isArray(enhet)) {
-		return enhet[0];
-	}
-
-	return enhet || undefined;
-}
-
-export function enhetFinnesIUrl() {
-	return !!hentEnhetIdFraUrl();
-}
-
-export function miljoFraUrl() {
-	return utledMiljoFraHost(location.host);
-}
-
-export function utledMiljoFraHost(host: string) {
-	const matches = host.match(/-[a-zA-Z][0-9]+/);
-	return matches == null ? '' : matches[0];
-}
-
 const BASE_URL = '/veilarbpersonflatefs/';
-
-export const refreshMedNyFnrIUrl = (fodselsnummer: string): void => {
-	window.location.pathname = `${BASE_URL}${fodselsnummer}`;
-};
 
 const regex = `^${BASE_URL}(\\d+)`;
 
