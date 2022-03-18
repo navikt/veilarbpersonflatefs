0bb6184c6cc599f68bf84de71190971e3c125843
Alexander Gård <alex.gaard@hotmail.com>
2022-03-18 09:54:39 +0100

    Forberedelser for deploy til GCP

--- craco.config.js
+++ craco.config.js
@@ -1,49 +1,5 @@
-const path = require('path');
-const CracoLessPlugin = require('craco-less');
-const BUILD_PATH = path.resolve(__dirname, './build');
-
-const removeCssHashPlugin = {
-    overrideWebpackConfig: ({ webpackConfig, cracoConfig, pluginOptions, context: { env, paths } }) => {
-
-        const plugins = webpackConfig.plugins;
-        plugins.forEach(plugin => {
-
-            const options = plugin.options;
-
-            if (!options) {
-                return;
-            }
-
-            if (options.filename && options.filename.endsWith('.css')) {
-                options.filename = "static/css/[name].css";
-            }
-
-        });
-
-        return webpackConfig;
-    }
-};
-
 module.exports = {
     plugins: [
-        { plugin: CracoLessPlugin },
-        { plugin: removeCssHashPlugin },
-    ],
-    webpack: {
-        configure: {
-            optimization: {
-                splitChunks: {
-                    cacheGroups: {
-                        default: false,
-                        vendors: false
-                    },
-                },
-                runtimeChunk: false
-            },
-            output: {
-                path: BUILD_PATH,
-                filename: 'static/js/[name].js',
-            },
-        }
-    }
+        { plugin: require('craco-less') },
+    ]
 };
