99946a1719145cbfcf1f4f54618d2a1c16a596fc
sigurdgroneng <sigurdgroneng@nav.no>
2022-09-01 13:33:58 +0200

    Extra env check

--- src/app.tsx
+++ src/app.tsx
@@ -6,11 +6,11 @@ import { hentFnrFraUrl } from './util/url-utils';
 import { PersonflatePage } from './page/personflate';
 import { synkroniserManuellStatusMedDkif } from './api/api';
 
-if (process.env.REACT_APP_NOT_SENTRY_ENV && process.env.REACT_APP_SENTRY_ENV !== 'development') {
+if (!process.env.REACT_APP_MOCK && process.env.REACT_APP_NOT_SENTRY_ENV) {
 	Sentry.init({
 		dsn: "https://82639012ef3d42aab4a8ac2d60e2c464@sentry.gc.nav.no/143",
 		integrations: [new BrowserTracing()],
-		environment: process.env.REACT_APP_SENTRY_ENV || 'environment-missing',
+		environment: process.env.REACT_APP_SENTRY_ENV,
 		// Set tracesSampleRate to 1.0 to capture 100%
 		// of transactions for performance monitoring.
 		// We recommend adjusting this value in production
