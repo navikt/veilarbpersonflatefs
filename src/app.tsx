34808de7761ed72b5c109c3b5afaaa16745edcb8
sigurdgroneng <sigurdgroneng@nav.no>
2022-09-01 13:30:44 +0200

    Use custom env-label for dev

--- src/app.tsx
+++ src/app.tsx
@@ -6,11 +6,11 @@ import { hentFnrFraUrl } from './util/url-utils';
 import { PersonflatePage } from './page/personflate';
 import { synkroniserManuellStatusMedDkif } from './api/api';
 
-if (process.env.NODE_ENV && process.env.NODE_ENV !== 'development') {
+if (process.env.REACT_APP_NOT_SENTRY_ENV && process.env.REACT_APP_SENTRY_ENV !== 'development') {
 	Sentry.init({
 		dsn: "https://82639012ef3d42aab4a8ac2d60e2c464@sentry.gc.nav.no/143",
 		integrations: [new BrowserTracing()],
-		environment: process.env.NODE_ENV || 'development',
+		environment: process.env.REACT_APP_SENTRY_ENV || 'environment-missing',
 		// Set tracesSampleRate to 1.0 to capture 100%
 		// of transactions for performance monitoring.
 		// We recommend adjusting this value in production
