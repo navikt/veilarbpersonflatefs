55ee7c8e5f934e5cf81aa1c080da2d57459f63ef
sigurdgroneng <sigurdgroneng@nav.no>
2022-09-02 15:36:49 +0200

    Require to avoid accidentally deploy of sentry to prod

--- src/app.tsx
+++ src/app.tsx
@@ -6,7 +6,7 @@ import { hentFnrFraUrl } from './util/url-utils';
 import { PersonflatePage } from './page/personflate';
 import { synkroniserManuellStatusMedDkif } from './api/api';
 
-if (process.env.REACT_APP_MOCK !== 'true' && process.env.REACT_APP_SENTRY_ENV) {
+if (process.env.REACT_APP_MOCK !== 'true' && !process.env.REACT_APP_SENTRY_ENV && process.env.REACT_APP_SENTRY_ENV !== 'prod') {
 	Sentry.init({
 		dsn: 'https://82639012ef3d42aab4a8ac2d60e2c464@sentry.gc.nav.no/143',
 		integrations: [new BrowserTracing()],
