34c5efc18a6cbea2811c0340f09613bcc0c5a396
sigurdgroneng <sigurdgroneng@nav.no>
2022-09-12 09:44:03 +0200

    Reduce sample rate

--- src/sentry.ts
+++ src/sentry.ts
@@ -68,6 +68,6 @@ Sentry.init({
 	// Set tracesSampleRate to 1.0 to capture 100%
 	// of transactions for performance monitoring.
 	// We recommend adjusting this value in production
-	tracesSampleRate: 1.0,
+	tracesSampleRate: 0.2,
 	beforeSend: fjernPersonopplysninger
 });
