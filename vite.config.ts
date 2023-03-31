5b187bc861647959ad7b78d4d1edbfdcc4c46218
sigurdgroneng <sigurd.groneng@nav.no>
2023-03-31 13:17:31 +0200

    React is nice

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	// config options,
+	plugins: [react()],
 	build: {
 		outDir: 'build'
 	},
