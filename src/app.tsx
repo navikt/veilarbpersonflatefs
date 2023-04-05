fd338966084ffb30e545515aac6183d73117a884
sigurdgroneng <sigurd.groneng@nav.no>
2023-04-05 10:19:53 +0200

    Dont store fnr in 2 places

--- src/app.tsx
+++ src/app.tsx
@@ -1,39 +1,10 @@
-import { useEffect, useState } from 'react';
 import StoreProvider from './store/store-provider';
-import { hentFnrFraUrl } from './util/url-utils';
 import { PersonflatePage } from './page/personflate';
-import { synkroniserManuellStatusMedDkif } from './api/api';
 import './sentry';
 
 export const App = () => {
-	const [fnr, setFnr] = useState(hentFnrFraUrl() || '');
-
-	useEffect(() => {
-		console.log('Fnr effect', { fnr });
-		const rerenderIfChangedFnr = (event: any) => {
-			const nextFnr = hentFnrFraUrl();
-			console.log('Popstate received', { nextFnr, fnr });
-			if (fnr === nextFnr || !nextFnr) return;
-			setFnr(nextFnr);
-		};
-		window.addEventListener('popstate', rerenderIfChangedFnr);
-		return () => window.removeEventListener('popstate', rerenderIfChangedFnr);
-	}, [fnr]);
-
-	useEffect(() => {
-		/*
-			Siden det ikke er mulig å få endringer på kontaktinfo fra DKIF asynkront, så må vi gjøre regelmessige spørringer for å holde dataen i synk.
-			Tidligere så ble dette gjort som en sideeffekt på et urelatert endepunkt, men det har nå blitt flyttet ut til et eget endepunkt.
-			Vi må derfor kalle dette endepunktet hver gang en veileder går inn på en bruker i veilarbpersonflatefs.
-			Det trengs ikke å vente på svar fra synkroniseringen før vi går videre med rendring av personflate.
-		 */
-		if (fnr) {
-			synkroniserManuellStatusMedDkif(fnr).catch();
-		}
-	}, [fnr]);
-
 	return (
-		<StoreProvider fnr={fnr}>
+		<StoreProvider>
 			<PersonflatePage />
 		</StoreProvider>
 	);
--- src/store/store-provider.tsx
+++ src/store/store-provider.tsx
@@ -1,28 +1,47 @@
 import React, { useEffect, useReducer } from 'react';
 import { useEventListener } from '../util/utils';
 import { createInitialStore, ModiaContext, reducer, SET_FNR, SET_RENDER_KEY } from './modia-context-store';
-import { a } from 'msw/lib/SetupServerApi-1855d9c6';
+import { hentFnrFraUrl } from '../util/url-utils';
+import { synkroniserManuellStatusMedDkif } from '../api/api';
 
 interface StoreProviderProps {
-	fnr: string;
 	children: React.ReactNode;
 }
 
-export const DispatchProvider = React.createContext((a: any) => {});
+export const DispatchProvider = React.createContext({});
 
 const StoreProvider = (props: StoreProviderProps) => {
-	const [state, dispatch] = useReducer(reducer, props.fnr, createInitialStore);
+	const [state, dispatch] = useReducer(reducer, hentFnrFraUrl() || '', createInitialStore);
+	const fnr = state.aktivBrukerFnr;
 
-	useEffect(() => {
-		console.log('Fnr prop changed');
-		if (state.aktivBrukerFnr !== props.fnr) {
-			dispatch({ type: SET_FNR, fnr: props.fnr });
-		}
-	}, [props.fnr]);
 	const forceRerender = () => dispatch({ type: SET_RENDER_KEY, renderKey: state.renderKey + 1 });
 	useEventListener('rerenderMao', forceRerender);
 	useEventListener('oppfolgingAvslutet', forceRerender);
 	useEventListener('eskaleringsVarselSendt', forceRerender);
+
+	useEffect(() => {
+		console.log('Fnr effect', { fnr });
+		const rerenderIfChangedFnr = (event: any) => {
+			const nextFnr = hentFnrFraUrl();
+			console.log('Popstate received', { nextFnr, fnr });
+			if (fnr === nextFnr || !nextFnr) return;
+			dispatch({ type: SET_FNR, fnr });
+		};
+		window.addEventListener('popstate', rerenderIfChangedFnr);
+		return () => window.removeEventListener('popstate', rerenderIfChangedFnr);
+	}, [fnr]);
+
+	useEffect(() => {
+		/*
+			Siden det ikke er mulig å få endringer på kontaktinfo fra DKIF asynkront, så må vi gjøre regelmessige spørringer for å holde dataen i synk.
+			Tidligere så ble dette gjort som en sideeffekt på et urelatert endepunkt, men det har nå blitt flyttet ut til et eget endepunkt.
+			Vi må derfor kalle dette endepunktet hver gang en veileder går inn på en bruker i veilarbpersonflatefs.
+			Det trengs ikke å vente på svar fra synkroniseringen før vi går videre med rendring av personflate.
+		 */
+		if (fnr) {
+			synkroniserManuellStatusMedDkif(fnr).catch();
+		}
+	}, [fnr]);
 
 	return (
 		<DispatchProvider.Provider value={dispatch}>
