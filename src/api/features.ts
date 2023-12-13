ad428fefa5ae1119dbd84310486bd63d84f68c79
sigurdgroneng <sigurd.groneng@nav.no>
2023-12-13 17:07:27 +0100

    Fix

--- src/api/features.ts
+++ src/api/features.ts
@@ -2,8 +2,8 @@ export const ARBEIDSMARKEDSTILTAK_LANSERING = 'mulighetsrommet-veileder-flate.la
 export const VIS_GAMLE_DETALJER_FANE = 'veilarbpersonflatefs.vis-gamle-detaljer-fane';
 export const DIALOG_WEBSOCKET = 'arbeidsrettet-dialog.websockets';
 
-export const OBO_UNLEASH_TOGGLES = [DIALOG_WEBSOCKET];
-export const DAB_UNLEASH_TOGGLES = [];
+export const OBO_UNLEASH_TOGGLES = [VIS_GAMLE_DETALJER_FANE];
+export const DAB_UNLEASH_TOGGLES = [DIALOG_WEBSOCKET];
 
 export interface OboUnleashFeatures {
 	[VIS_GAMLE_DETALJER_FANE]: boolean;
