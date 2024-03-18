ccfdcdc37888549a4c6f8b62679b18498ad5a581
Magnus Evensberget <magnus.evensberget@nav.no>
2024-03-18 09:47:56 +0100

    Fjerner obligatorisk tiltaksgjennomføringid for Deltaker

--- src/page/tiltak/TiltakPage.tsx
+++ src/page/tiltak/TiltakPage.tsx
@@ -1,7 +1,6 @@
 import { useLoadDeltakerRegistreringApp } from './hooks/useLoadDeltakerRegistreringApp';
 import { useModiaContext } from '../../store/modia-context-store';
 import { createElement } from 'react';
-import { Alert } from '@navikt/ds-react';
 
 const TiltakPage = () => {
 
@@ -11,14 +10,6 @@ const TiltakPage = () => {
 	const tiltaksgjennomforingId = searchParams.get('id');
 
 	const { aktivBrukerFnr, aktivEnhetId } = useModiaContext();
-
-	if (tiltaksgjennomforingId === null) {
-		return (
-			<div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-				<Alert variant="error">Klarte ikke laste deltakerregistrering</Alert>
-			</div>
-		);
-	}
 
 	return (
 		createElement('arbeidsmarkedstiltak-deltaker', {
