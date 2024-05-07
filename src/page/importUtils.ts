b700e04acef99a62e9832ddc8f5e264d55dc105d
sigurdgroneng <sigurd.groneng@nav.no>
2024-05-07 09:50:28 +0200

    Dont import apps twice

--- src/page/importUtils.ts
+++ src/page/importUtils.ts
@@ -2,7 +2,11 @@ interface Manifest {
 	'index.html': { file: string };
 }
 
+const importedApps: Record<string, boolean> = {};
+
 export const importSubApp = (url: string) => {
+	if (url in importedApps) return;
+	importedApps[url] = true;
 	return fetch(`${url}/asset-manifest.json`)
 		.then(res => res.json())
 		.then((manifest: Manifest) => {
