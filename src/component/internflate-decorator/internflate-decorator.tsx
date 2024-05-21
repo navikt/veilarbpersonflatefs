947593f1e0f72954fa6a08fc5868b02fea466f2b
sigurdgroneng <sigurd.groneng@nav.no>
2024-05-21 08:48:04 +0200

    Use proxt /modiacontextholder

--- src/component/internflate-decorator/internflate-decorator.tsx
+++ src/component/internflate-decorator/internflate-decorator.tsx
@@ -40,8 +40,8 @@ function lagDecoratorConfig(
 		enhet: enhetId,
 		onEnhetChanged: newEnhet => props.onEnhetChanged(newEnhet || null),
 		onFnrChanged: newFnr => props.onFnrChanged(newFnr || null),
-		useProxy: false,
-		// proxy: '/',
+		useProxy: true,
+		proxy: '/modiacontextholder',
 		environment: getEnv() === 'prod' ? 'prod' : 'q1',
 		fetchActiveUserOnMount: true,
 		fetchActiveEnhetOnMount: true
