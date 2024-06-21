8991ca5fe80ac62b6f57c8c6a2ba36ae9f970770
JulieHillRoa <julie.hill.roa@nav.no>
2024-06-21 11:20:20 +0200

    abort

--- src/component/internflate-decorator/internflate-decorator.tsx
+++ src/component/internflate-decorator/internflate-decorator.tsx
@@ -36,7 +36,7 @@ function getDecoratorEnv(): DecoratorEnvironment {
 
 function lagDecoratorConfig(
 	props: InternflateDecoratorProps
-): DecoratorConfigV2 {
+): DecoratorConfigV2 & { proxy: string; useProxy: boolean } {
 	const fnr = props.fnr ?? undefined;
 	const enhetId = props.enhetId ?? undefined;
 	return {
@@ -44,6 +44,8 @@ function lagDecoratorConfig(
 		enhet: enhetId,
 		onEnhetChanged: newEnhet => props.onEnhetChanged(newEnhet ?? null),
 		onFnrChanged: newFnr => props.onFnrChanged(newFnr ?? null),
+		useProxy: true,
+		proxy: '/modiacontextholder',
 		environment: getDecoratorEnv(),
 		fetchActiveUserOnMount: true,
 		fetchActiveEnhetOnMount: true,
