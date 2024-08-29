bb41c3b9a4819dba92ea5c6d499b34ab4628849b
sigurdgroneng <sigurd.groneng@nav.no>
2024-08-29 12:26:34 +0200

    Ikke ha spinner i personflate

--- src/page/personflate.tsx
+++ src/page/personflate.tsx
@@ -1,13 +1,12 @@
 import { useEffect } from 'react';
 import { useFetchFeaturesFromOboUnleash, useFetchTilgangTilBruker } from '../api/api';
-import { hasAnyFailed, isAnyLoading } from '../api/utils';
+import { hasAnyFailed } from '../api/utils';
 import {
 	FeilUnderLastingAvData,
 	FeilmeldingManglerFnr,
 	IngenTilgangTilBruker
 } from '../component/alertstriper/alertstriper';
 import { InternflateDecorator } from '../component/internflate-decorator/internflate-decorator';
-import PageSpinner from '../component/page-spinner/page-spinner';
 import SideInnhold from '../component/side-innhold';
 import { UtloptSesjonAdvarsel } from '../component/utlopt-sesjon-advarsel/utlopt-sesjon-advarsel';
 import { SesjonStatus, useSesjonStatus } from '../hooks/use-sesjon-status';
@@ -16,21 +15,20 @@ import { applications } from '../data/applications';
 import { dispatchNavigateEvent } from '../Router';
 
 export const PersonflatePage = () => {
-	const { aktivBrukerFnr, aktivEnhetId, setAktivEnhetId, setAktivBrukerFnr } =
-		useModiaContext();
+	const { aktivBrukerFnr, aktivEnhetId, setAktivEnhetId, setAktivBrukerFnr } = useModiaContext();
 	const { sesjonStatus } = useSesjonStatus();
 
 	const fetchTilgangTilBruker = useFetchTilgangTilBruker(aktivBrukerFnr, { manual: true });
 	const fetchOboUnleashFeatures = useFetchFeaturesFromOboUnleash();
 
 	const onAktivBrukerChanged = (newFnr: string | null) => {
-		if(!newFnr) return;
+		if (!newFnr) return;
 		setAktivBrukerFnr(newFnr);
-		const application = applications.find(app => window.location.pathname.startsWith(app.pathEntrypoint))
-		if(application) {
-			dispatchNavigateEvent(application.pathEntrypoint)
+		const application = applications.find(app => window.location.pathname.startsWith(app.pathEntrypoint));
+		if (application) {
+			dispatchNavigateEvent(application.pathEntrypoint);
 		}
-	}
+	};
 
 	const onAktivEnhetChanged = (newEnhetId: string | null) => {
 		if (newEnhetId && newEnhetId !== aktivEnhetId) {
@@ -49,8 +47,6 @@ export const PersonflatePage = () => {
 
 	if (!aktivBrukerFnr) {
 		innhold = <FeilmeldingManglerFnr />;
-	} else if (isAnyLoading(fetchTilgangTilBruker, fetchOboUnleashFeatures)) {
-		innhold = <PageSpinner />;
 	} else if (hasAnyFailed(fetchTilgangTilBruker, fetchOboUnleashFeatures)) {
 		innhold = <FeilUnderLastingAvData />;
 	} else if (!fetchTilgangTilBruker.data) {
