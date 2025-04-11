58f3a654b85728910e3b73518386eff25455cb54
sigurdgroneng <sigurd.groneng@nav.no>
2025-04-11 15:33:33 +0200

    Add sentry vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 export default defineConfig({
 	// config options,
-	plugins: [react()],
+	plugins: [
+		react(),
+		sentryVitePlugin({
+			org: 'nav',
+			project: 'veilarbpersonflate',
+
+			// Auth tokens can be obtained from https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/
+			authToken: process.env.SENTRY_AUTH_TOKEN
+		})
+	],
 	build: {
 		outDir: 'build',
 		sourcemap: true
