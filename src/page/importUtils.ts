fe9bf59addaa68c92d4bd456632765ee39d55498
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-05-06 08:01:02 +0200

    returner asset-manifest fra importSubApp

--- src/page/importUtils.ts
+++ src/page/importUtils.ts
@@ -1,15 +1,30 @@
-interface Manifest {
+export interface Manifest {
 	'index.html': { file: string };
 }
 
-const importedApps: Record<string, boolean> = {};
+const appCache = new Map<string, Promise<Manifest>>();
 
-export const importSubApp = (url: string) => {
-	if (url in importedApps) return;
-	importedApps[url] = true;
-	return fetch(`${url}/asset-manifest.json`)
-		.then(res => res.json())
-		.then((manifest: Manifest) => {
-			import(/* @vite-ignore */ `${url}/${manifest['index.html'].file}`);
-		});
-};
+export function importSubApp(url: string): Promise<Manifest> {
+	const cachedPromise = appCache.get(url);
+	if (cachedPromise) {
+		return cachedPromise;
+	}
+
+	const importPromise = new Promise<Manifest>((resolve, reject) => {
+		fetch(`${url}/asset-manifest.json`)
+			.then(res => res.json())
+			.then((manifest: Manifest) => {
+				return import(/* @vite-ignore */ `${url}/${manifest['index.html'].file}`).then(() => {
+					resolve(manifest);
+				});
+			})
+			.catch(error => {
+				appCache.delete(url);
+				reject(error);
+			});
+	});
+
+	appCache.set(url, importPromise);
+
+	return importPromise;
+}
