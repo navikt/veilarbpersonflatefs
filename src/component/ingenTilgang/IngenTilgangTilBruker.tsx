d637a63a3e9e730047c4f4d682931700d2e1436e
Mads.Lee.Giil <mads.lee.giil@nav.no>
2026-03-10 08:44:07 +0100

    Unngå at veileder får knapp for flytting av bruker etter å ha flyttet bruker

--- src/component/ingenTilgang/IngenTilgangTilBruker.tsx
+++ src/component/ingenTilgang/IngenTilgangTilBruker.tsx
@@ -8,6 +8,7 @@ import { dispatchNavigateEvent } from '../../Router';
 import { EnvType, getEnv } from '../../util/utils';
 import { logAnalyticsEvent } from '../../analytics/analytics';
 import { hentVeilederOgEnheter, HentVeilederOgEnheterResponse, settKontor } from '../../api/api';
+import useHarFlyttetBrukerTilEgetKontor from '../../hooks/useHarFlyttetBrukerTilEgetKontor';
 
 enum Steg {
 	IKKE_STARTET,
@@ -18,6 +19,7 @@ enum Steg {
 
 export const IngenTilgangTilBruker = () => {
 	const { aktivBrukerFnr, aktivEnhetId } = useModiaContext();
+	const { harFlyttetBrukerTilEgetKontor, settAtVeilederHarFlyttetBrukerTilEgetKontor } = useHarFlyttetBrukerTilEgetKontor(aktivBrukerFnr);
 	const [tilgangFlytteBrukerEgetKontor, setTilgangFlytteBrukerEgetKontor] = useState<boolean | undefined>();
 	const [harAktiveTiltaksdeltakelser, setHarAktiveTiltaksdeltakelser] = useState<boolean | undefined>();
 	const [veilederOgEnheter, setVeilederOgEnheter] = useState<HentVeilederOgEnheterResponse | undefined>();
@@ -57,6 +59,7 @@ export const IngenTilgangTilBruker = () => {
 		const result = await settKontor(aktivBrukerFnr, aktivEnhetId);
 		if (result) {
 			setSteg(Steg.HAR_ENDRET_KONTOR);
+			settAtVeilederHarFlyttetBrukerTilEgetKontor();
 			logAnalyticsEvent('knapp klikket', { tekst: 'flyttet-bruker-til-veileders-kontor' });
 		}
 	};
@@ -64,68 +67,75 @@ export const IngenTilgangTilBruker = () => {
 	const featureErSkruddPaa = getEnv().type === EnvType.dev || getEnv().type === EnvType.local
 	const aktivEnhetNavn = veilederOgEnheter?.enheter.find(enhet => enhet.enhetId === aktivEnhetId)?.navn;
 	const lasterAktivEnhetNavn = aktivEnhetNavn === undefined;
+	const skalViseMeldingOmAtBrukerAlleredeErFlyttetTilEgetKontor = harFlyttetBrukerTilEgetKontor && steg !== Steg.HAR_ENDRET_KONTOR;
 
 	return (
 		<div className="ingen-tilgang-container">
 			<IngenTilgangTilBrukerAlertStripe />
 
-			<div className="ingen-tilgang">
-				{featureErSkruddPaa && tilgangFlytteBrukerEgetKontor && (
-					<div>
-						{(steg === Steg.ENDRER_KONTOR || steg === Steg.HAR_IKKE_ENDRET_KONTOR) &&
-							(lasterAktivEnhetNavn ? (
-								<div className="ingen-tilgang-innhold">
-									<Skeleton variant="rectangle" height={60} />
-									<Skeleton
-										variant="rounded"
-										className="ingen-tilgang-knapp"
-										height={40}
-										width={240}
-									/>
-								</div>
-							) : (
-								<div className="ingen-tilgang-innhold">
-									<Heading
-										size={'medium'}
-										className={'ingen-tilgang-heading'}
+			{ skalViseMeldingOmAtBrukerAlleredeErFlyttetTilEgetKontor  ?
+				<div className="ingen-tilgang">
+					Du har allerede flyttet bruker til eget kontor. Det vil ta minst en halvtime før du får tilgang til bruker.
+				</div>
+			:
+				<div className="ingen-tilgang">
+					{featureErSkruddPaa && tilgangFlytteBrukerEgetKontor && (
+						<div>
+							{(steg === Steg.ENDRER_KONTOR || steg === Steg.HAR_IKKE_ENDRET_KONTOR) &&
+								(lasterAktivEnhetNavn ? (
+									<div className="ingen-tilgang-innhold">
+										<Skeleton variant="rectangle" height={60} />
+										<Skeleton
+											variant="rounded"
+											className="ingen-tilgang-knapp"
+											height={40}
+											width={240}
+										/>
+									</div>
+								) : (
+									<div className="ingen-tilgang-innhold">
+										<Heading
+											size={'medium'}
+											className={'ingen-tilgang-heading'}
+										>
+											Ikke tilgang til bruker
+										</Heading>
+										<BodyShort>
+											Du har ikke tilgang til bruker, men kan flytte bruker til {aktivEnhetNavn}. Du
+											vil da få tilgang til bruker.
+										</BodyShort>
+										{harAktiveTiltaksdeltakelser && <Alert variant={'info'} className={'ingen-tilgang-alert'}>
+											Bruker deltar på tiltak. Hvis du flytter brukeren må du undersøke om dette kan
+											få konsekvenser for tiltaksdeltakelsen.
+										</Alert>}
+										<Button
+											className="ingen-tilgang-knapp"
+											loading={steg === Steg.ENDRER_KONTOR}
+											onClick={settKontorButtonClicked}
+											variant={'secondary'}
+										>
+											Flytt bruker til {aktivEnhetNavn}
+										</Button>
+									</div>
+								))}
+							{steg === Steg.HAR_ENDRET_KONTOR && (
+								<div>
+									<InlineMessage status="success" size="medium">
+										Brukers arbeidsoppfølgingskontor er nå {aktivEnhetNavn}
+									</InlineMessage>
+									<Link
+										className="ingen-tilgang-link"
+										href="/aktivitetsplan"
+										onClick={() => dispatchNavigateEvent('/aktivitetsplan')}
 									>
-										Ikke tilgang til bruker
-									</Heading>
-									<BodyShort>
-										Du har ikke tilgang til bruker, men kan flytte bruker til {aktivEnhetNavn}. Du
-										vil da få tilgang til bruker.
-									</BodyShort>
-									{harAktiveTiltaksdeltakelser && <Alert variant={'info'} className={'ingen-tilgang-alert'}>
-										Bruker deltar på tiltak. Hvis du flytter brukeren må du undersøke om dette kan
-										få konsekvenser for tiltaksdeltakelsen.
-									</Alert>}
-									<Button
-										className="ingen-tilgang-knapp"
-										loading={steg === Steg.ENDRER_KONTOR}
-										onClick={settKontorButtonClicked}
-										variant={'secondary'}
-									>
-										Flytt bruker til {aktivEnhetNavn}
-									</Button>
+										Gå til aktivitetsplanen
+									</Link>
 								</div>
-							))}
-						{steg === Steg.HAR_ENDRET_KONTOR && (
-							<div>
-								<InlineMessage status="success" size="medium">
-									Brukers arbeidsoppfølgingskontor er nå {aktivEnhetNavn}
-								</InlineMessage>
-								<Link
-									className="ingen-tilgang-link"
-									href="/aktivitetsplan"
-									onClick={() => dispatchNavigateEvent('/aktivitetsplan')}
-								>
-									Gå til aktivitetsplanen
-								</Link>
-							</div>
-						)}
-					</div>
-				)}
-			</div>
+							)}
+						</div>
+					)}
+				</div>
+			}
 		</div>
 	);
 };
