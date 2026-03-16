39b9d07630bddfb7edc95eabef68f93bd851b687
Mads.Lee.Giil <mads.lee.giil@nav.no>
2026-03-16 13:40:46 +0100

    Unngå at info om flyttet bruker forsvinner fra browser når man navigerer til andre sider

---
+++ src/store/flyttet-bruker-store.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+
+const useHarFlyttetBrukerTilEgetKontor = (brukerFnr: string) => {
+	const keyPrefix = 'harFlyttetBrukerTilEgetKontor';
+	const flyttetBrukerTilEgetKontorKey = `${keyPrefix}-${brukerFnr}`;
+	const [harFlyttetBrukerTilEgetKontor, setHarFlyttetBrukerTilEgetKontor] = useState<boolean>(window.localStorage.getItem(flyttetBrukerTilEgetKontorKey) != null);
+
+	const registrerAtVeilederHarFlyttetBrukerTilEgetKontor = () => {
+		setHarFlyttetBrukerTilEgetKontor(true);
+		localStorage.setItem(flyttetBrukerTilEgetKontorKey, new Date().getTime().toString());
+	}
+
+	const slettUtlopteInnslagAvFlyttetBrukerTilEgetKontor = () => {
+		const now = new Date().getTime();
+		const enTimeMillis = 3600000;
+		const lagredeItems = Object.keys(localStorage).filter((key) => key.startsWith(keyPrefix));
+		lagredeItems.forEach((key) => {
+			const timestamp = Number(localStorage[key]);
+			const harUtlopt = (now - enTimeMillis) > timestamp;
+			if (harUtlopt) {
+				localStorage.removeItem(key);
+			}
+		})
+	}
+
+	return { harFlyttetBrukerTilEgetKontor, setHarFlyttetBrukerTilEgetKontor: registrerAtVeilederHarFlyttetBrukerTilEgetKontor, slettUtlopteInnslagAvFlyttetBrukerTilEgetKontor };
+}
+
+export default useHarFlyttetBrukerTilEgetKontor;
