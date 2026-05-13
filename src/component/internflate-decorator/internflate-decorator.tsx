8a75b4d240a78fab1eebe6d6da59b87fe85d70e0
Mads.Lee.Giil <mads.lee.giil@nav.no>
2026-05-13 08:56:23 +0200

    WIP: Bruk dekoratøren som webcomponent

--- src/component/internflate-decorator/internflate-decorator.tsx
+++ src/component/internflate-decorator/internflate-decorator.tsx
@@ -1,20 +1,12 @@
 import { DecoratorConfigV2, DecoratorEnvironment } from './internflate-decorator-config';
-import NAVSPA from '@navikt/navspa';
-import { EnvType, SpaName, getEnv } from '../../util/utils';
-import { useContext, useMemo } from 'react';
+import { EnvType, getEnv } from '../../util/utils';
+import { useContext, useLayoutEffect, useMemo, useRef } from 'react';
 import { DispatchContext } from '../../store/store-provider';
 
 interface InternflateDecoratorProps {
 	onEnhetChanged: (newEnhet: string | undefined | null) => void;
 	onFnrChanged: (newFnr: string | undefined | null) => void;
 }
-
-export const Decorator: React.ComponentType<DecoratorConfigV2> = NAVSPA.importer(
-	SpaName.INTERNARBEIDSFLATEFS_DECORATOR,
-	{
-		wrapperClassName: ''
-	}
-);
 
 export function InternflateDecorator() {
 	const dispatch = useContext(DispatchContext);
@@ -25,10 +17,40 @@ export function InternflateDecorator() {
 		});
 	}, []);
 
+	const decoratorRef = useRef<HTMLElement>(null);
+
+	useLayoutEffect(() => {
+		const decoratorElement = decoratorRef.current;
+		if (!decoratorElement) return;
+		const handleFnrChanged = (e: Event) => {
+			const fnr = (e as CustomEvent).detail.fnr;
+			decoratorProps.onFnrChanged(fnr);
+		};
+		const handleEnhetChanged = (e: Event) => {
+			const enhet = (e as CustomEvent).detail.enhet;
+			decoratorProps.onEnhetChanged(enhet);
+		}
+		decoratorElement.addEventListener('fnr-changed', handleFnrChanged);
+		decoratorElement.addEventListener('enhet-changed', handleEnhetChanged);
+		return () => {
+			decoratorElement.removeEventListener('fnr-changed', handleFnrChanged);
+			decoratorElement.removeEventListener('enhet-changed', handleFnrChanged);
+		};
+	}, []);
+
 	return (
-		<nav>
-			<Decorator {...lagDecoratorConfig(decoratorProps)} />
-		</nav>
+		<internarbeidsflate-decorator
+			ref={decoratorRef}
+			app-name="Arbeidsrettet oppfølging"
+			environment={getEnv().type === EnvType.prod ? 'prod' : 'q2'}
+			url-format={getEnv().ingressType === 'ansatt' ? 'ANSATT' : 'NAV_NO'}
+			show-enheter="false"
+			show-search-area="true"
+			fetch-active-enhet-on-mount="false"
+			fetch-active-user-on-mount="true"
+			show-hotkeys="false"
+			proxy={'/api/modiacontextholder'}
+		/>
 	);
 }
 
