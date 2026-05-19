c029b35e908a5ce769ab5e7fcff8be3995938002
sigurdgroneng <sigurd.groneng@nav.no>
2026-05-19 11:45:08 +0200

    Revert "Bruk dekoratøren som webcomponent (#710)"
    This reverts commit f676829fefe500861a1d24e817a2732c82a40447.

--- src/component/internflate-decorator/internflate-decorator.tsx
+++ src/component/internflate-decorator/internflate-decorator.tsx
@@ -1,42 +1,60 @@
-import { EnvType, getEnv } from '../../util/utils';
-import { useContext, useLayoutEffect, useRef } from 'react';
+import { DecoratorConfigV2, DecoratorEnvironment } from './internflate-decorator-config';
+import NAVSPA from '@navikt/navspa';
+import { EnvType, SpaName, getEnv } from '../../util/utils';
+import { useContext, useMemo } from 'react';
 import { DispatchContext } from '../../store/store-provider';
+
+interface InternflateDecoratorProps {
+	onEnhetChanged: (newEnhet: string | undefined | null) => void;
+	onFnrChanged: (newFnr: string | undefined | null) => void;
+}
+
+export const Decorator: React.ComponentType<DecoratorConfigV2> = NAVSPA.importer(
+	SpaName.INTERNARBEIDSFLATEFS_DECORATOR,
+	{
+		wrapperClassName: ''
+	}
+);
 
 export function InternflateDecorator() {
 	const dispatch = useContext(DispatchContext);
-	const decoratorRef = useRef<HTMLElement>(null);
-
-	useLayoutEffect(() => {
-		const decoratorElement = decoratorRef.current;
-		if (!decoratorElement) return;
-		const handleFnrChanged = (e: Event) => {
-			const fnr = (e as CustomEvent).detail.fnr;
-			dispatch({ type: 'ON_AKTIV_BRUKER_CHANGED', fnr });
-		};
-		const handleEnhetChanged = (e: Event) => {
-			const enhet = (e as CustomEvent).detail.enhet;
-			dispatch({ type: 'ON_AKTIV_ENHET_CHANGED', enhet });
-		}
-		decoratorElement.addEventListener('fnr-changed', handleFnrChanged);
-		decoratorElement.addEventListener('enhet-changed', handleEnhetChanged);
-		return () => {
-			decoratorElement.removeEventListener('fnr-changed', handleFnrChanged);
-			decoratorElement.removeEventListener('enhet-changed', handleEnhetChanged);
-		};
+	const decoratorProps = useMemo(() => {
+		return lagDecoratorConfig({
+			onEnhetChanged: enhet => dispatch({ type: 'ON_AKTIV_ENHET_CHANGED', enhet }),
+			onFnrChanged: fnr => dispatch({ type: 'ON_AKTIV_BRUKER_CHANGED', fnr })
+		});
 	}, []);
 
 	return (
-		<internarbeidsflate-decorator
-			ref={decoratorRef}
-			app-name="Arbeidsrettet oppfølging"
-			environment={getEnv().type === EnvType.prod ? 'prod' : 'q2'}
-			url-format={getEnv().ingressType === 'ansatt' ? 'ANSATT' : 'NAV_NO'}
-			show-enheter="true"
-			show-search-area="true"
-			fetch-active-enhet-on-mount="true"
-			fetch-active-user-on-mount="true"
-			show-hotkeys="false"
-			proxy="/modiacontextholder"
-		/>
+		<nav>
+			<Decorator {...lagDecoratorConfig(decoratorProps)} />
+		</nav>
 	);
+}
+
+function getDecoratorEnv(): DecoratorEnvironment {
+	const env = getEnv();
+	if (env.type === EnvType.prod) {
+		return 'prod';
+	} else {
+		return 'q2';
+	}
+}
+
+function lagDecoratorConfig(
+	props: InternflateDecoratorProps
+): DecoratorConfigV2 & { proxy: string; useProxy: boolean } {
+	return {
+		fnr: undefined,
+		enhet: undefined,
+		onEnhetChanged: newEnhet => props.onEnhetChanged(newEnhet ?? null),
+		onFnrChanged: newFnr => props.onFnrChanged(newFnr ?? null),
+		useProxy: true,
+		proxy: '/modiacontextholder',
+		environment: getDecoratorEnv(),
+		fetchActiveUserOnMount: true,
+		fetchActiveEnhetOnMount: true,
+		urlFormat: getEnv().ingressType === 'ansatt' ? 'ANSATT' : 'NAV_NO',
+		appName: 'Arbeidsrettet oppfølging'
+	};
 }
