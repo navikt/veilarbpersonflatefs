19df82bd1600cac1b546d43e433ed1c39ec2aef3
sigurdgroneng <sigurd.groneng@nav.no>
2026-05-26 14:10:47 +0200

    Use ao-kontor toggle from unleash

--- src/api/features.ts
+++ src/api/features.ts
@@ -1,7 +1,8 @@
 export const DIALOG_WEBSOCKET = 'veilarbpersonflatefs.dialog.websockets';
-
+export const BRUK_AO_KONTOR_SOM_MASTER = 'bruk_ao_kontor_som_master';
 export const DAB_UNLEASH_TOGGLES = [DIALOG_WEBSOCKET];
 
 export interface DabUnleashFeatures {
 	[DIALOG_WEBSOCKET]: boolean;
+	[BRUK_AO_KONTOR_SOM_MASTER]: boolean;
 }
