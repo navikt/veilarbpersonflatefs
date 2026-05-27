690b53afb0b1ab9f1ea35bb5273ec8b8feed6770
sigurdgroneng <sigurd.groneng@nav.no>
2026-05-27 10:59:50 +0200

    Legg til Bruk ao kontor som master toggle i liste over toggles

--- src/api/features.ts
+++ src/api/features.ts
@@ -1,6 +1,6 @@
 export const DIALOG_WEBSOCKET = 'veilarbpersonflatefs.dialog.websockets';
 export const BRUK_AO_KONTOR_SOM_MASTER = 'bruk_ao_kontor_som_master';
-export const DAB_UNLEASH_TOGGLES = [DIALOG_WEBSOCKET];
+export const DAB_UNLEASH_TOGGLES = [DIALOG_WEBSOCKET, BRUK_AO_KONTOR_SOM_MASTER];
 
 export interface DabUnleashFeatures {
 	[DIALOG_WEBSOCKET]: boolean;
