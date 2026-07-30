566f5bc4f89a24ef95e6838fc327751994788203
Sanne Edland <sanneed@hotmail.com>
2026-07-30 14:36:03 +0200

    FinnStillinger darkmode theme håndtert

--- src/page/FinnStillingerPage.tsx
+++ src/page/FinnStillingerPage.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AsyncNavspa, AsyncSpaConfig } from '@navikt/navspa';
 import type { SpaProps } from '../component/spa';
 import { SpaName, spaWrapperTabContentClassName } from '../util/utils';
 import { utledSpaUrl } from '../util/url-utils';
 import { useModiaContext } from '../store/modia-context-store';
 import Spinner from '../component/spinner/spinner';
+
+type Theme = 'light' | 'dark';
+
+type FinnStillingProps = SpaProps & {
+	theme: Theme;
+};
 
 const finnStillingInngangAsyncConfig: AsyncSpaConfig = {
 	appName: SpaName.FINN_STILLING_INNGANG,
@@ -24,13 +30,32 @@ const finnStillingInngangAsyncConfig: AsyncSpaConfig = {
 	}
 };
 
-const FinnStillingInngang: React.ComponentType<SpaProps> =
-	AsyncNavspa.importer<SpaProps>(finnStillingInngangAsyncConfig);
+const FinnStillingInngang: React.ComponentType<FinnStillingProps> =
+	AsyncNavspa.importer<FinnStillingProps>(finnStillingInngangAsyncConfig);
 
 const FinnStillingerPage = () => {
 	const { aktivBrukerFnr, aktivEnhetId } = useModiaContext();
+	const [theme, setTheme] = useState<Theme>('light');
 
-	return <FinnStillingInngang fnr={aktivBrukerFnr} enhet={aktivEnhetId ?? undefined} />;
+	useEffect(() => {
+		const syncTheme = () => {
+			setTheme(document.body.classList.contains('dark') ? 'dark' : 'light');
+		};
+
+		syncTheme();
+
+		const observer = new MutationObserver(syncTheme);
+		observer.observe(document.body, {
+			attributes: true,
+			attributeFilter: ['class']
+		});
+
+		return () => {
+			observer.disconnect();
+		};
+	}, []);
+
+	return <FinnStillingInngang fnr={aktivBrukerFnr} enhet={aktivEnhetId ?? undefined} theme={theme} />;
 };
 
 export default FinnStillingerPage;
