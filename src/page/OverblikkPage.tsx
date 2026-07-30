fadbefb9373fd4e2b8abb1ef3db9e1113f8a7bae
Sanne Edland <56392377+sanneed@users.noreply.github.com>
2026-07-30 12:08:30 +0200

    Legg til Themehåndtering i Overblikk (#730)

--- src/page/OverblikkPage.tsx
+++ src/page/OverblikkPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { utledOboCdnUrl } from '../util/url-utils';
 import { useModiaContext } from '../store/modia-context-store';
 import { importSubApp } from './importUtils';
@@ -7,12 +7,44 @@ const overblikkBaseUrl = utledOboCdnUrl('veilarbdetaljerfs/dist');
 
 const OverblikkPage = () => {
 	const { aktivBrukerFnr } = useModiaContext();
+	const overblikkElementRef = useRef<HTMLElement>(null);
 
 	useEffect(() => {
 		importSubApp(overblikkBaseUrl);
 	}, []);
 
-	return React.createElement('veilarb-detaljer', { ['data-fnr']: aktivBrukerFnr });
+	useEffect(() => {
+		const overblikkElement = overblikkElementRef.current;
+
+		if (!overblikkElement) {
+			return;
+		}
+
+		const syncTheme = () => {
+			const theme = document.body.classList.contains('dark') ? 'dark' : 'light';
+
+			if (overblikkElement.getAttribute('theme') !== theme) {
+				overblikkElement.setAttribute('theme', theme);
+			}
+		};
+
+		syncTheme();
+
+		const observer = new MutationObserver(syncTheme);
+		observer.observe(document.body, {
+			attributes: true,
+			attributeFilter: ['class']
+		});
+
+		return () => {
+			observer.disconnect();
+		};
+	}, []);
+
+	return React.createElement('veilarb-detaljer', {
+		ref: overblikkElementRef,
+		['data-fnr']: aktivBrukerFnr
+	});
 };
 
 export default OverblikkPage;
