963daff2241efed399d72cce803ed64e8ed5e78f
Tia Firing <tia.firing@nav.no>
2026-09-14 08:09:05 +0200

    tydeliggjør at det tar en halvtime før man får tilgang

--- src/component/ingenTilgang/IngenTilgangStartOppfolging.tsx
+++ src/component/ingenTilgang/IngenTilgangStartOppfolging.tsx
@@ -23,7 +23,7 @@ export const IngenTilgangStartOppfolging = ({ aktivEnhetNavn }: IngenTilgangStar
 				onChange={() => setHarHuketAvForAStarteOppfolging(!harHuketAvForAStarteOppfolging)}
 				className="flytt-bruker-checkbox"
 			>
-				Ja, bruker skal følges opp av {aktivEnhetNavn} (det tar minst en halvtime før du får tilgang til bruker)
+				Ja, bruker skal følges opp av {aktivEnhetNavn} (det tar minst en <b>halvtime</b> før du får tilgang til bruker)
 			</Checkbox>
 			<Button
 				disabled={!harHuketAvForAStarteOppfolging}
